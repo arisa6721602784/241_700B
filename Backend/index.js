bf2fb24f1384fa9b3c458bf36c01a1f438f5117f
arisa6721602784 <152466351+SBenyaporn@users.noreply.github.com>
2026-02-13 12:19:13 +0700

    add api get post put delete

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,4 @@
-//ทำการ import โมดูล http
+/**ทำการ import โมดูล http
 const http = require('http');
 const host = 'localhost';
 const port = 8000;
@@ -13,4 +13,87 @@ const requestListener = function (req, res) {
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
+});*/
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 8000;
+
+let users = [];
+let counter = 1;
+
+/**
+    GET /users => ดึงข้อมูลผู้ใช้ทั้งหมด
+    POST /user => เพิ่มผู้ใช้ใหม่
+    GET /user/:id => ดึงข้อมูลผู้ใช้ตาม id
+    PUT /user/:id => แก้ไขข้อมูลผู้ใช้ตาม id ที่บันทึก
+    DELETE /user/:id => ลบผู้ใช้ตาม id ที่บันทึก
+ */
+
+//path: = /GET /users
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+//path: = POST /user
+app.post('/user', (req, res) => {
+    let user = req.body;
+    user.id = counter
+    counter += 1;
+
+    users.push(user);
+    res.json({
+    Message: 'User added successfully',
+    user: user
+    });
+});
+
+// path: = PUT /user/:id
+app.patch('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let updatedUser = req.body;
+
+    //หา user ที่จาก id ที่ส่งมา
+    let selectedIndex = users.findIndex(user => user.id == id);
+
+    //อัปเดตข้อมูล users
+    if (updatedUser.firstname) {
+        users[selectedIndex].firstname = updatedUser.firstname;
+    }
+    if (updatedUser.lastname) {
+        users[selectedIndex].lastname = updatedUser.lastname;
+    }
+
+    res.json({
+        Message: 'User updated successfully',
+        data: {
+            user: updatedUser,
+            indexupdate: selectedIndex    
+        }
+    });
+    //ส่ง users ที่อัปเดตแล้วกลับไป
+});
+
+app.delete('/users/:id', (req, res) => {
+    let id = req.params.id;
+    //หา index จาก id ที่ต้องการลบ
+    let selectedIndex = users.findIndex(user => user.id == id);
+
+    //ลบ user ออกจาก users
+    users.splice(selectedIndex, 1);
+
+    res.json({
+            Message: 'User deleted successfully',
+            indexupdate: selectedIndex
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
